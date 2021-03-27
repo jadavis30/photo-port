2298fff1277248d3b5de87b1bb8344a69a0b1887
Jared Davis <jared.davis21@yahoo.com>
2021-03-27 13:10:41 -0400

    fixed classname on navigation

--- src/App.js
+++ src/App.js
@@ -11,19 +11,19 @@ function App() {
       description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
     { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    { name: 'food ', description: 'Delicious delicacies' },
+    { name: 'landscape ', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
-      <Nav>
+      <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      </Nav>
+      ></Nav>
       <main>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
